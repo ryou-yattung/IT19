35fdcf9c2374eee368629bdbe00292d555f4a7ae
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2021-12-29 10:32:30 +0900

    IT19

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// $(function(){
+//      var bg = $(".bg_warp img");
+//      var bgImg = -1;
+//      bg.hide();
+
+//      function showNextTxt(){
+//           bgImg++;
+//           bg.eq(bgImg % bg.length).fadeIn(1000).animate({transition: '0.1s',transform:" scaleY(1.2)", opacity: 0},1000).fadeOut(00, showNextTxt);
+//           // bg.eq(bgImg % bg.length).fadeIn(1000).delay(2000).fadeOut(1000, showNextTxt);
+//           // bg.animate({transition: '2s', left: '-50px',},1000);
+
+//      }
+//      showNextTxt();
+
+//      // var bg = $(".bg_warp img");
+//      // var bgImg = -1;
+//      // bg++;
+
+//      // var bgImgNum = bgImg % bg.length;
+//      // var next = (bgImg + 1) % txts.length;
+//      // var current = bg[bgImgNum];
+//      // var next = bg[next];
+
+//      // setTimeout(function(){
+//      //      current.css({})
+
+//      // });
+
+
+// });
